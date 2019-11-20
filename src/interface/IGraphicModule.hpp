777eb03db9753d3c8bfac148657d09323e0153d0
cousin_t <tom.cousin@epitech.eu>
2019-11-20 22:27:40 +0100

    Add beginning of GraphicModule and GameEngine with associated interface

---
+++ src/interface/IGraphicModule.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2022
+** CPP_rtype_2019
+** File description:
+** Created by tomcousin,
+*/
+
+#ifndef CPP_RTYPE_2019_IGRAPHICMODULE_HPP
+#define CPP_RTYPE_2019_IGRAPHICMODULE_HPP
+
+namespace rtype {
+
+    namespace engine {
+
+        namespace module {
+
+            class IGraphicModule {
+
+                // @MARK Public
+                public:
+
+                // @MARK Destructor
+                virtual ~IGraphicModule() = default;
+            };
+        }
+    }
+}
+
+#endif //CPP_RTYPE_2019_IGRAPHICMODULE_HPP
