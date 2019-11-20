b4672a1d2c3b5ab5248c7320509626b1cc75ea62
cousin_t <tom.cousin@epitech.eu>
2019-11-20 19:56:48 +0100

    First commit, add CMakeList with Conan and basic source for client and server program

---
+++ src/client/main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** CPP_rtype_2019
+** File description:
+** Created by tomcousin,
+*/
+
+/*
+ * @brief Main function for the r-type-client-program
+ *
+ * @return 0 if no error occurred
+ */
+int main(void)
+{
+    return 0;
+}
