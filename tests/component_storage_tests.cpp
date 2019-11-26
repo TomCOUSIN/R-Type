98188f463f0d2a611c8f9cd773f7415db6d282c7
cousin_t <tom.cousin@epitech.eu>
2019-11-26 09:47:16 +0100

    Update tests according to latest change

--- tests/component_storage_tests.cpp
+++ tests/component_storage_tests.cpp
@@ -33,26 +33,3 @@ Test(component_storage_tests, test_invalid_component_insertion)
     storage.addEntity(entity, component::Component());
     cr_assert_not(storage.addEntity(entity, component::Component()));
 }
-
-/**
- * @brief Test to get the Component linked to an Entity
- */
-Test(component_storage_tests, test_get_component)
-{
-    component::ComponentStorage<component::Component> storage;
-    entity::Entity entity = 0;
-
-    storage.addEntity(entity, component::Component());
-    cr_assert_not_null(storage.getComponent(entity));
-}
-
-/**
- * @brief Test to get when the Entity isn't linked to a Component
- */
-Test(component_storage_tests, test_invalid_get_component)
-{
-    component::ComponentStorage<component::Component> storage;
-    entity::Entity entity = 0;
-
-    cr_assert_null(storage.getComponent(entity));
-}
